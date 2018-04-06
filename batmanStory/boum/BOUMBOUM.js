2f63704eecd9b1de143df0dcf81230307122e0bc
Christopher Fremond <christopher.fremond@gmail.com>
2018-04-06 08:27:00 +0200

    storyBatmaninProgress

---
+++ batmanStory/boum/BOUMBOUM.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".element", {
+  strings: ['These are the default values...', 'You know what you should do?', 'Use your own!', 'Have a great day!'],
+  stringsElement: null,
+  typeSpeed: 0,
+  startDelay: 0,
+  backSpeed: 0,
+  smartBackspace: true,
+});
